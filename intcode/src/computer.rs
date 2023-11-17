6ac58ac00a8cca180975d10e09573e314fa93119
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2023-11-17 13:14:12 +0100

    add input and output channels to computer

--- intcode/src/computer.rs
+++ intcode/src/computer.rs
@@ -1,3 +1,5 @@
+use crossbeam_channel::{Receiver, Sender};
+
 use crate::{
     error::Error,
     error::Result,
@@ -12,13 +14,23 @@ use crate::{
 pub struct Computer {
     pub(crate) memory: Memory,
     pub(crate) instruction_pointer: usize,
+    input_tx: Sender<Word>,
+    input_rx: Receiver<Word>,
+    output_tx: Sender<Word>,
+    output_rx: Receiver<Word>,
 }
 
 impl Computer {
     pub fn new(program: impl Into<Memory>) -> Self {
+        let (input_tx, input_rx) = crossbeam_channel::bounded(0);
+        let (output_tx, output_rx) = crossbeam_channel::bounded(0);
         Self {
             memory: program.into(),
             instruction_pointer: 0,
+            input_tx,
+            input_rx,
+            output_tx,
+            output_rx,
         }
     }
 
@@ -86,5 +98,68 @@ impl Computer {
 
     pub fn into_memory(self) -> Vec<Word> {
         self.memory.into_inner()
+    }
+
+    /// Get a sender for the input channel.
+    pub fn input(&self) -> Sender<Word> {
+        self.input_tx.clone()
+    }
+
+    /// Provide a set of inputs to the input channel.
+    ///
+    /// This is most useful before the computer is running, given a known set of inputs.
+    ///
+    /// ```rust,ignore
+    /// let mut computer = Computer::new(program);
+    /// computer.provide_input([1]);
+    /// computer.run()?;
+    /// ```
+    pub fn provide_input(&self, inputs: impl 'static + IntoIterator<Item = Word> + Send) {
+        let sender = self.input();
+        std::thread::spawn(move || {
+            for word in inputs.into_iter() {
+                if sender.send(word).is_err() {
+                    // receiver disconnected; end thread
+                    return;
+                }
+            }
+        });
+    }
+
+    /// Get a receiver for the output channel.
+    pub fn output(&self) -> Receiver<Word> {
+        self.output_rx.clone()
+    }
+
+    /// Collect all outputs from the output channel.
+    ///
+    /// This function implicitly runs `Self` to completion.
+    pub fn collect_outputs<Collection>(&mut self) -> Result<Collection>
+    where
+        Collection: Default + Extend<Word> + Send,
+    {
+        std::thread::scope(|scope| {
+            let mut collection = Collection::default();
+            let output = self.output();
+
+            let collector_handle = scope.spawn(move || {
+                // an error indicates that the sender has disconnected
+                while let Ok(word) = output.recv() {
+                    collection.extend(std::iter::once(word));
+                }
+                collection
+            });
+            let runner_handle = scope.spawn(|| self.run());
+
+            runner_handle
+                .join()
+                .unwrap_or_else(|panic| std::panic::resume_unwind(panic))?;
+
+            let collection = collector_handle
+                .join()
+                .unwrap_or_else(|panic| std::panic::resume_unwind(panic));
+
+            Ok(collection)
+        })
     }
 }
