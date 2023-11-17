fd057a441dc7bd16913c8a6a19757708c0a578d0
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2023-11-17 16:38:23 +0100

    impl day05 part 2

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -2,34 +2,42 @@ use aoclib::{parse, CommaSep};
 use intcode::{Computer, Word};
 use std::{ops::Deref, path::Path};
 
-pub fn part1(input: &Path) -> Result<(), Error> {
-    for (idx, program) in parse::<CommaSep<Word>>(input)?.enumerate() {
-        let program = program.deref().clone();
-        let mut computer = Computer::new(program);
+fn get_diagnostic_code(program: Vec<Word>, device_id: Word) -> Result<Word, Error> {
+    let mut computer = Computer::new(program);
 
-        computer.provide_input([1]);
-        let outputs = computer.collect_outputs::<Vec<_>>()?;
+    computer.provide_input([device_id]);
+    let outputs = computer.collect_outputs::<Vec<_>>()?;
 
-        let [test_codes @ .., diagnostic_code] = outputs.as_slice() else {
-            return Err(Error::NoSolution);
-        };
+    let [test_codes @ .., diagnostic_code] = outputs.as_slice() else {
+        return Err(Error::NoSolution);
+    };
 
-        for (idx, test_code) in test_codes.iter().enumerate() {
-            if *test_code != 0 {
-                return Err(Error::TestFailure {
-                    idx,
-                    value: *test_code,
-                });
-            }
+    for (idx, test_code) in test_codes.iter().enumerate() {
+        if *test_code != 0 {
+            return Err(Error::TestFailure {
+                idx,
+                value: *test_code,
+            });
         }
+    }
 
-        println!("pgm {idx} pt 1: {diagnostic_code}");
+    Ok(*diagnostic_code)
+}
+
+fn run_programs(input: &Path, part: u8, device_id: Word) -> Result<(), Error> {
+    for (idx, program) in parse::<CommaSep<Word>>(input)?.enumerate() {
+        let diagnostic_code = get_diagnostic_code(program.deref().clone(), device_id)?;
+        println!("pgm {idx} pt {part}: {diagnostic_code}");
     }
     Ok(())
 }
 
+pub fn part1(input: &Path) -> Result<(), Error> {
+    run_programs(input, 1, 1)
+}
+
 pub fn part2(input: &Path) -> Result<(), Error> {
-    todo!()
+    run_programs(input, 2, 5)
 }
 
 #[derive(Debug, thiserror::Error)]
